97118af1707efe56d495b2544f5aa55cfc5a7c1f
202335300ParkJY <163634389+202335300ParkJY@users.noreply.github.com>
2024-06-04 12:17:26 +0900

    Add files via upload

---
+++ week14-1/week14-1/package0/module0.py
@@ -0,0 +1,3 @@
+
+print(f'module0: {__file__ = }')
+print(f'module0: {__name__ = }')
---
+++ week14-1/week14-1/test.py
@@ -0,0 +1,6 @@
+
+print(f'module0: {__file__ = }')
+print(f'module0: {__name__ = }')
+
+# from package0 import module0
+import package0.module0
